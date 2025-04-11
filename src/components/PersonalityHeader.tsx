7236e5c5575d317e085079c2b34617f10334f0da
PritamMajumder-MFSI <pritamm@mindfiresolutions.com>
2025-04-11 17:37:18 +0530

    fix

--- src/components/PersonalityHeader.tsx
+++ src/components/PersonalityHeader.tsx
@@ -11,9 +11,9 @@ const PersonalityTestHeader: FC = () => {
         }}
         src="/logo.png"
         alt="Gravity CVG Logo"
-        className="w-48 cursor-pointer h-auto mb-8 animate-float transition-transform duration-300 hover:scale-105 hover:rotate-3"
+        className="w-44 cursor-pointer h-auto mb-8 animate-float transition-transform duration-300 hover:scale-105 hover:rotate-3"
       />
-      <h1 className="text-3xl md:text-4xl font-bold mb-4 bg-gradient-to-r from-cyan-400 to-purple-300 bg-clip-text text-transparent drop-shadow-[0_5px_35px_rgba(255,255,255,0.5)]">
+      <h1 className="text-2xl md:text-3xl font-bold mb-4 bg-gradient-to-r from-cyan-400 to-purple-300 bg-clip-text text-transparent drop-shadow-[0_5px_35px_rgba(255,255,255,0.5)]">
         Personality Test
       </h1>
       <div className="relative w-64 h-1 mb-6 overflow-hidden">
